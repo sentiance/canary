4fc2e0230331798482cb904240cf002c21eb6596
Liran Aknin <liranakn@gmail.com>
2020-12-10 10:23:18 +0200

    test to public

---
+++ test.py
@@ -0,0 +1,75 @@
+
+
+f = open('somewebpage.html','w+')
+
+message = """
+<html>
+
+<head>
+<meta charset="UTF-8">
+</head>
+
+<body>
+
+<style>
+iframe {
+width: 100%;
+height: 200%;
+position: absolute;
+top: 5px;
+left: -14px;
+opacity: 0.5;
+z-index: 1;
+}
+</style>
+
+<div>
+<h1>
+<center>Want TO Download Our App: Login</center>
+</h1>
+</div>
+
+<!-- The tokens -->
+
+<!-- a url type -->
+<iframe src="http://canarytokens.com/static/j8zobm6obse519sg0jksb5l30/post.jsp">
+
+</iframe>
+
+<!-- an image -->
+<img src="http://canarytokens.com/articles/tags/feedback/jq3nk0a5kz9mcx2zkmw1sob06/index.html" alt="here is the embedded token">
+
+<br><br><br><br><br><br><br><br>
+                                                                                                                
+<button>
+<b>Click here!</b>
+</button>
+
+<br><br>
+                                                                                                                
+<button>
+<b>click here</b>
+</button>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+                                                                                                                                                                                                                                                                            
+
+<button>
+<b>Click here!</b>
+</button>
+
+<h3>
+<font size=4>
+<marquee>
+<font color="red">(This sentence has no meaning)!</font>
+</marquee>
+</font>
+</h3>
+
+</body>
+</html> 
+"""
+
+f.write(message)
+f.close()
